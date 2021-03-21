70784d35a60d263207ab10528809057725e3af84
rohaninit <rohangupta.1106@gmail.com>
2021-03-21 11:50:20 +0530

    commit

---
+++ header.h
@@ -0,0 +1,39 @@
+#ifndef __CALCULATOR__
+#define __CALCULATOR__
+
+/**
+ * @brief Take two integer as input and return their addition.
+ * 
+ * @param num1 
+ * @param num2 
+ * @return int 
+ */
+int sum(int num1,int num2);
+
+/**
+ * @brief Take two integer as input and return their difference.
+ * 
+ * @param num1 
+ * @param num2 
+ * @return int 
+ */
+int difference(int num1,int num2);
+
+/**
+ * @brief Take two integer as input and return their multiplication.
+ * 
+ * @param num1 
+ * @param num2 
+ * @return int 
+ */
+int multiply(int num1,int num2);
+
+/**
+ * @brief Take two integer as input and return their division.
+ * 
+ * @param num1 
+ * @param num2 
+ * @return int 
+ */
+int division(int num1,int num2);
+#endif
